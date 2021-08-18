a77d001f98a3a2c4d3442bb9dca610c9d78daadc
Piumi Ranawansha <piumiranawansha@gmail.com>
2021-08-19 01:06:14 +0530

    Search Page Modified

--- client/src/components/pages/SearchCars.js
+++ client/src/components/pages/SearchCars.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import Header from '../../Header';
 import Footer from '../../Footer';
+import SearchCarsCss from '../../css/SearchCars.css';
 
 export default class SearchCars extends React.Component{
 
@@ -10,6 +11,21 @@ export default class SearchCars extends React.Component{
             <div>
                 <Header />
 
+
+                <h1>Search Results</h1>
+
+                <div className="input-group" id="measure1" >
+                    <div className="form-outline">
+                        <input id="search-focus" type="search" id="form1" className="form-control" placeholder="Search"/>
+                    </div>
+                    <button type="button" className="btn btn-primary">
+                        <i className="fa fa-search"></i>
+                    </button>
+                </div>
+
+                <div id="measure2">
+
+                </div>
 
 
                 <Footer />
